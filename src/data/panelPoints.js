c3bb0291d14635b4c7c12a330e23512b1ce4fa77
Aidan Brus <aidanbrus@gmail.com>
2025-12-01 20:02:21 -0700

    e1201-WidePointFunction

--- src/data/panelPoints.js
+++ src/data/panelPoints.js
@@ -6,17 +6,30 @@ import * as THREE from 'three';
 import camPoints from './panelMidpoint.json'; 
 
 const up = new THREE.Vector3(0, 0, 1);
-
+// NEED TO CREATE JSON WITH CAMERA DIRECTIONS TO SCALE PANELS INFRONT OF THE CAMERA
 camPoints.forEach(row => {
     const pos = new THREE.Vector3(row.x, row.y, row.z);
 
     if (row.type === "split") {
         const wing = pos.clone().cross(up.clone());
-        const pointR  = wing.clone().add(new THREE.Vector3(0, 0, 5));
+        const opWing = up.clone().cross(pos.clone());
+
+        const tempPoint  = pos.clone().add(new THREE.Vector3(0, 0, 5));
+        const tempPointOp = pos.clone().add(new THREE.Vector3(0, 0, 5));
+
+        const tempPoint2 = tempPoint.clone().add(wing.clone());
+        const tempPointOp2 = tempPointOp.clone().add(opWing.clone());
+
+        // need to push the points forward by adding  the scaled camera direction vector to push panels forward
+
+        // then add both points to the main new json file
+    } else {
+        const tempPt = pos.clone().add(new THREE.Vector3(0, 0, 5));
+        // const midPt = tempPT.clone().add(// insert camera direction vector here )
+
+        // add point to the new json file
     }
 })
 
 
-// writeFileSync('panelCenters_wide.json', JSON.stringify( {wideCenters}, null, 2));
-// writeFileSync('panelCenters_skinny.json', JSON.stringify( {skinnyCenters}, null, 2));
 
